a7f53d0b808674633a90d245fadf8aee1d2eddf0
jasonz3157 <jasonz3157@gmail.com>
2024-03-05 13:44:19 +0800

    import libs first

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import os
 import sys
 from datetime import datetime
 
-from flowlauncher import FlowLauncher
-
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, "lib"))
 sys.path.append(os.path.join(parent_folder_path, "plugin"))
+
+from flowlauncher import FlowLauncher
 
 
 class DateDiff(FlowLauncher):
