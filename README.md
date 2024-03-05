13637968ccaa4e611b67ffa50b1048465de23a7b
jasonz3157 <jasonz3157@gmail.com>
2024-03-05 11:49:01 +0800

    add DateDiff.query

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Flow.Launcher.Plugin.DateDiffPy
 A FlowLauncher plugin written in python for calculating date diff
+
+> There is already an awesome datediff plugin [Flow.Launcher.Plugin.DateDiff](https://github.com/LeoDupont/Flow.Launcher.Plugin.DateDiff), but it requires nodejs to be installed.<br>I don't want to install nodejs on my PC, so I wrote this simple python plugin that uses the flow's embedded python without any other dependencies.
---
+++ main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from datetime import datetime
+
+from flowlauncher import FlowLauncher
+
+parent_folder_path = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(parent_folder_path)
+sys.path.append(os.path.join(parent_folder_path, "lib"))
+sys.path.append(os.path.join(parent_folder_path, "plugin"))
+
+
+class DateDiff(FlowLauncher):
+    def query(self, arguments: str):
+        args = arguments.strip().split(" ")
+        if not args:
+            return
+        if len(args) == 1:
+            to_given = args[0]
+            from_dt = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        else:
+            to_given = args[1]
+            from_given = args[0]
+            from_dt = datetime.strptime(f"{from_given:08}", "%Y%m%d")
+        to_dt = datetime.strptime(f"{to_given:08}", "%Y%m%d")
+
+        diff = (to_dt - from_dt).days
+        return [
+            {
+                "Title": f"{diff} Days",
+                "SubTitle": f"From {from_dt:%Y%m%d} to {to_dt:%Y%m%d}",
+                "IcoPath": "Images/app.png",
+            }
+        ]
+
+
+if __name__ == "__main__":
+    DateDiff()
