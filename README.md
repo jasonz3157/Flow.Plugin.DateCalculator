1f16e6d10695ffe22ea67f7317fb685acdcf9eeb
jasonz3157 <jasonz3157@gmail.com>
2025-03-11 16:34:36 +0800

    resolve #2 - support more date formats； - support date math unit

--- README.md
+++ README.md
@@ -1,33 +1,59 @@
-# Flow.Launcher.Plugin.DateCalculator
+# Flow.Plugin.DateCalculator
 A [FlowLauncher](https://github.com/Flow-Launcher/Flow.Launcher) plugin written in python for calculating date
 
 > [!TIP]
 > There is already an awesome [Flow.Launcher.Plugin.DateDiff](https://github.com/LeoDupont/Flow.Launcher.Plugin.DateDiff) plugin, but it requires nodejs to be installed. I don't want to install nodejs on my PC, so I wrote this simple python plugin that uses Flow's embedded python.
 
 ### Usage
+
+> [!TIP]
+> This plugin use python-dateutil to parse date string.
+> It supports many formats, see [dateutil's documentation](https://dateutil.readthedocs.io/en/stable/examples.html#parse-examples) for details.
+
 #### Diff
-
-<img src="https://s2.loli.net/2024/03/07/oxOzmCsUMZ2l1Ja.png" width="400">
-
 ```
-dc [FROM_DATE] TO_DATE
+dc [FROM_DATE] TO_DATE [%UNIT]
 ```
-If *FROM_DATE* is not provided, today's date will be used.
+- If *FROM_DATE* is not provided, today's date will be used.
+- If *FROM_DATE* contains space, please wrap it with quotes.
+- *%UNIT* is optional. *UNIT* can be one of the following:
+    - `s`/`S`: seconds.
+    - `M`: minutes.
+    - `h`/`H`: hours.
+    - `d`/`D`: days, default.
+    - `m`: months.
+    - `y`/`Y`: years.
 
 e.g.
 ```
 dc 20250101
+dc 2025/01/01 %m
 dc 2025/01/01
-dc 2025-01-01
-dc 20240101 20250101
-dc 2024/01/01 2025/01/01
-dc 2024-01-01 2025-01-01
+dc "Oct. 10, 2025" %H
+dc 20240101 2025-01-01
+dc 2024/01/01 "Oct 11 2024"
 ```
+
+<img src="https://github.com/jasonz3157/Flow.Plugin.DateCalculator/blob/master/Images/diff01.png?raw=true" width="400">
+
+<img src="https://github.com/jasonz3157/Flow.Plugin.DateCalculator/blob/master/Images/diff02.png?raw=true" width="400">
+
+<img src="https://github.com/jasonz3157/Flow.Plugin.DateCalculator/blob/master/Images/diff03.png?raw=true" width="400">
+
+<img src="https://github.com/jasonz3157/Flow.Plugin.DateCalculator/blob/master/Images/diff04.png?raw=true" width="400">
+
+<img src="https://github.com/jasonz3157/Flow.Plugin.DateCalculator/blob/master/Images/diff05.png?raw=true" width="400">
+
+<img src="https://github.com/jasonz3157/Flow.Plugin.DateCalculator/blob/master/Images/diff06.png?raw=true" width="400">
+
 #### Calculating
 
 ```
 dc [FROM_DATE] {+|-}{TIME_DELTA}
 ```
+- If *FROM_DATE* is not provided, today's date will be used.
+- If *FROM_DATE* contains space, please wrap it with quotes.
+
 e.g.
 ```
 dc +181d
@@ -38,5 +64,10 @@ dc "Mar. 11, 2025" -1y
 ```
 <img src="https://github.com/jasonz3157/Flow.Plugin.DateCalculator/blob/master/Images/calculate01.png?raw=true" width="400">
 
-- If *FROM_DATE* is not provided, today's date will be used.
-- If *FROM_DATE* contains space, please wrap it with quotes.
+<img src="https://github.com/jasonz3157/Flow.Plugin.DateCalculator/blob/master/Images/calculate02.png?raw=true" width="400">
+
+<img src="https://github.com/jasonz3157/Flow.Plugin.DateCalculator/blob/master/Images/calculate03.png?raw=true" width="400">
+
+<img src="https://github.com/jasonz3157/Flow.Plugin.DateCalculator/blob/master/Images/calculate04.png?raw=true" width="400">
+
+<img src="https://github.com/jasonz3157/Flow.Plugin.DateCalculator/blob/master/Images/calculate05.png?raw=true" width="400">
--- main.py
+++ main.py
@@ -4,89 +4,139 @@ import os
 import re
 import sys
 from datetime import datetime
+import shlex
+import locale
+import ctypes
 
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, "lib"))
 sys.path.append(os.path.join(parent_folder_path, "plugin"))
 
-from dateutil.relativedelta import *
-from flowlauncher import FlowLauncher
+from dateutil.relativedelta import relativedelta  # noqa: E402
+from dateutil.parser import parse  # noqa: E402
+from flowlauncher import FlowLauncher  # noqa: E402
 
 
 class DateCalculator(FlowLauncher):
     def query(self, arguments: str):
         if not arguments:
             return
-        args = [str(_).lower() for _ in arguments.strip().split(" ")]
+        try:
+            args = shlex.split(arguments)
+        except ValueError:
+            return
 
-        for fmt in ["%Y%m%d", "%Y-%m-%d", "%Y/%m/%d"]:
-            to_given = args[0] if len(args) == 1 else args[1]
-            from_given = (
-                datetime.strftime(datetime.now(), fmt) if len(args) == 1 else args[0]
-            )
+        now = datetime.now()
 
-            # dd [date] {+|-}1y2m3w4d
-            if to_given.startswith("+") or to_given.startswith("-"):
+        _locale = locale.windows_locale[
+            ctypes.windll.kernel32.GetUserDefaultUILanguage()
+        ]
+        locale.setlocale(locale.LC_ALL, _locale)
+        for arg in args:
+            if arg.startswith(("-", "+")):
+                # Calculating mode
+                time_delta = arg
                 try:
-                    from_dt = datetime.strptime(from_given, fmt)
-                except ValueError:
-                    continue
+                    from_dt = parse(args[0])
+                except Exception:
+                    from_dt = now
                 delta_groups = re.search(
-                    r"(?P<operator>[-+])(?P<year>\d+y)?(?P<month>\d+m)?(?P<week>\d+w)?(?P<day>\d+d)?",
-                    to_given,
-                    flags=re.IGNORECASE,
+                    r"(?P<operator>[-+])(?P<years>\d+[yY])?(?P<months>\d+m)?(?P<weeks>\d+[wW])?(?P<days>\d+[dD])?(?P<hours>\d+[Hh])?(?P<minutes>\d+M)?(?P<seconds>\d+[sS])?",
+                    time_delta,
                 )
                 operator = delta_groups.group("operator")
-                target_dt = from_dt + relativedelta(
-                    years=(
-                        int(operator + delta_groups.group("year").rstrip("y"))
-                        if delta_groups.group("year")
-                        else 0
-                    ),
-                    months=(
-                        int(operator + delta_groups.group("month").rstrip("m"))
-                        if delta_groups.group("month")
-                        else 0
-                    ),
-                    weeks=(
-                        int(operator + delta_groups.group("week").rstrip("w"))
-                        if delta_groups.group("week")
-                        else 0
-                    ),
-                    days=(
-                        int(operator + delta_groups.group("day").rstrip("d"))
-                        if delta_groups.group("day")
-                        else 0
-                    ),
-                )
-                if target_dt == from_dt:
-                    return
+                deltas = {
+                    "years": int(
+                        operator + re.sub("[yY]", "", delta_groups.group("years"))
+                    )
+                    if delta_groups.group("years")
+                    else 0,
+                    "months": int(
+                        operator + re.sub("[m]", "", delta_groups.group("months"))
+                    )
+                    if delta_groups.group("months")
+                    else 0,
+                    "weeks": int(
+                        operator + re.sub("[wW]", "", delta_groups.group("weeks"))
+                    )
+                    if delta_groups.group("weeks")
+                    else 0,
+                    "days": int(
+                        operator + re.sub("[dD]", "", delta_groups.group("days"))
+                    )
+                    if delta_groups.group("days")
+                    else 0,
+                    "hours": int(
+                        operator + re.sub("[hH]", "", delta_groups.group("hours"))
+                    )
+                    if delta_groups.group("hours")
+                    else 0,
+                    "minutes": int(
+                        operator + re.sub("[M]", "", delta_groups.group("minutes"))
+                    )
+                    if delta_groups.group("minutes")
+                    else 0,
+                    "seconds": int(
+                        operator + re.sub("[sS]", "", delta_groups.group("seconds"))
+                    )
+                    if delta_groups.group("seconds")
+                    else 0,
+                }
+                target_dt = from_dt + relativedelta(**deltas)
+                return [
+                    {
+                        "Title": f"{target_dt:%x %X}",
+                        "SubTitle": f"{from_dt:%x %X} {time_delta}",
+                        "IcoPath": "Images/app.png",
+                    }
+                ]
+
+        for index, arg in enumerate(args):
+            if arg.startswith("%"):
+                if arg.lstrip("%") in list("sSMhHdDmyY"):
+                    time_fmt = arg.lstrip("%")
                 else:
-                    return [
-                        {
-                            "Title": f"{target_dt:%Y-%m-%d}",
-                            "SubTitle": f"{from_dt:%Y-%m-%d} {to_given}",
-                            "IcoPath": "Images/app.png",
-                        }
-                    ]
-            # dd [from] to
-            else:
-                try:
-                    from_dt = datetime.strptime(from_given, fmt)
-                    to_dt = datetime.strptime(to_given, fmt)
-                except ValueError:
-                    continue
-                else:
-                    diff = (to_dt - from_dt).days
-                    return [
-                        {
-                            "Title": str(diff),
-                            "SubTitle": f"{from_dt:%Y-%m-%d} → {to_dt:%Y-%m-%d}",
-                            "IcoPath": "Images/app.png",
-                        }
-                    ]
-        return
+                    time_fmt = "d"
+                args.pop(index)
+                break
+        else:
+            time_fmt = "d"
+
+        try:
+            to_date = parse(args[-1], default=now)
+        except Exception:
+            return
+        try:
+            from_date = parse(args[-2], default=now)
+        except Exception:
+            from_date = now
+
+        relativedelta_diff = relativedelta(to_date, from_date)
+        remain_months = abs(relativedelta_diff.months)
+        remain_days = abs(relativedelta_diff.days)
+
+        delta_diff = to_date - from_date
+
+        if time_fmt in ["d", "D"]:
+            diff = f"{delta_diff.days} days"
+        elif time_fmt in ["y", "Y"]:
+            diff = f"{relativedelta_diff.years} years {remain_months} months {remain_days} days"
+        elif time_fmt in ["m"]:
+            diff = f"{relativedelta_diff.years * 12 + relativedelta_diff.months} months {remain_days} days"
+        elif time_fmt in ["h", "H"]:
+            diff = f"{delta_diff.days * 24} hours"
+        elif time_fmt in ["M"]:
+            diff = f"{delta_diff.days * 24 * 60} minutes"
+        elif time_fmt in ["s", "S"]:
+            diff = f"{delta_diff.total_seconds} seconds"
+        return [
+            {
+                "Title": diff,
+                "SubTitle": f"{now:%x %X} → {to_date:%x %X}",
+                "IcoPath": "Images/app.png",
+            }
+        ]
 
 
 if __name__ == "__main__":
